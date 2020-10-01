48cd62e44180af12dd7852f8d5f9c05335411b2a
Donna Engelberger <dengelberger@gmail.com>
2020-10-01 11:22:22 -0400

    as far as we got in class.

--- server.js
+++ server.js
@@ -16,19 +16,19 @@ app.use(express.json());
 
 var tables = [
     {
-        name: "name",
-        phone: "phone",
-        mail: "email",
-        id: "id"
+        customerName: "name",
+        phoneNumber: "phone",
+        customerEmail: "email",
+        customerID: "id"
     }
 ];
 
 var waitList = [
     {
-        name: "name",
-        phone: "phone",
-        email: "email",
-        id: "id"
+      customerName: "name",
+      phoneNumber: "phone",
+      customerEmail: "email",
+      customerID: "id"
     }
 ];
 //ROUTES
@@ -37,67 +37,58 @@ var waitList = [
 
 // Basic route that sends the user first to the AJAX Page
 
+//the form to fill out for a table reservation
 app.get("/reserve", function (req, res) {
     res.sendFile(path.join(__dirname, "reserve.html"));
 });
-
+//shows the tables array
 app.get("/tables", function (req, res) {
     res.sendFile(path.join(__dirname, "tables.html"));
 });
-
+//shows the main page where you can click tables or reservation.
 app.get("/", function (req, res) {
     res.sendFile(path.join(__dirname, "home.html"));
 });
-
+//shows the waitlist on the tables page after showing the current tables reserved.
+app.get("/api/waitlist", function (req, res) {
+  res.sendFile(path.join(__dirname, "tables.html"));
+});
 // Create a new reservation based upon user input:
 
 app.post("/api/tables", function(req, res) {
     // req.body hosts is equal to the JSON post sent from the user
     // This works because of our body parsing middleware
- 
-    console.log(req.body.newReservation);
+    console.log(req.body);
+
+    if(tables.length<5) {
   
-    tables.push(req.body.newReservation);
-  
-    res.json(req.body.newReservation);
+    tables.push(req.body);
+  } else {
+    waitlist.push(req.body);
+  }
+
+    res.json(req.body);
+
   });
+
   app.get("/api/tables", function(req, res) {
+    
+    
     // req.body hosts is equal to the JSON post sent from the user
     // This works because of our body parsing middleware
 
-  
-
     console.log(tables);
   
     res.json(tables);
   });
 
-  
+  app.get("/api/waitlist", function(req, res) {
+    // req.body hosts is equal to the JSON post sent from the user
+    // This works because of our body parsing middleware
 
-    console.log(tables);
+    console.log(waitlist);
   
-    res.json(tables);
-  });
-
-  
-
-    console.log(tables);
-  
-    res.json(tables);
-  });
-
-  
-
-    console.log(tables);
-  
-    res.json(tables);
-  });
-
-  
-
-    console.log(tables);
-  
-    res.json(tables);
+    res.json(waitlist);
   });
 
 // Starts the server to begin listening
